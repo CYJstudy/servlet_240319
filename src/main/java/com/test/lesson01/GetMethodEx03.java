96d857ea0b34441baa6d53ac4f8c898c8076b50e
DESKTOP-AN805P4\user <test@test.com>
2024-05-29 16:16:18 +0900

    스태시 테스트

--- src/main/java/com/test/lesson01/GetMethodEx03.java
+++ src/main/java/com/test/lesson01/GetMethodEx03.java
@@ -14,6 +14,7 @@ public class GetMethodEx03 extends HttpServlet {
 	@Override
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 		
+		// stsch 테스트 주석 추가
 		// 테스트용 주석 추가
 		
 		response.setCharacterEncoding("utf-8");
